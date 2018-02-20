e0a8078949f8ad333ebf80c5ec145eea6257ab09
Pablo  S. Sanchez Arnoletto <pablo@dixis.es>
2018-02-20 18:46:33 +0100

    creacion de auth0 app

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { HousePage } from '../pages/house/house';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import {  AuthService } from '../providers/auth/auth';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    HousePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    HousePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
+  ]
+})
+export class AppModule {}
